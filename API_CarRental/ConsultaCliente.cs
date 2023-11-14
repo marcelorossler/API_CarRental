e13034757cd7a75d5fc8b16cbb4b8cde882cc7fc
Diego Papale <diegopapale@gmail.com>
2023-11-14 18:38:40 -0300

    criação Controllers API 14.11

---
+++ API_CarRental/ConsultaCliente.cs
@@ -0,0 +1,11 @@
+﻿namespace API_CarRental
+{
+    public class ConsultaCliente
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string nascimento { get; set; }
+        public string endereco { get; set; }
+    }
+}
---
+++ API_CarRental/Veiculo.cs
@@ -0,0 +1,11 @@
+﻿namespace API_CarRental
+{
+    public class Veiculo
+    {
+        public int codigo { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string cor { get; set; }
+        public string placa { get; set; }    
+    }
+}
